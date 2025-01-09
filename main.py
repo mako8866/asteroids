1a0fe44dc53687a6bdec61448997767794edc690
mako8866 <mako8866@gmail.com>
2025-01-08 20:08:02 -0500

    Added Player Update: Lvl5 Ch3

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main ():
         
               pygame.Surface.fill(screen,(0,0,0))
               player.draw(screen)
+              player.update(dt)
               pygame.display.flip()
               dt = clock.tick(60) / 1000
         print("Starting asteroids!")
